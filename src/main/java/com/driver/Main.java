6e98b3d8019e285ea0af3e42a5f781bf72fb7c91
nares <naresh170999@gmail.com>
2023-08-11 22:53:21 +0530

    submit again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,19 +3,22 @@ package com.driver;
 public class Main {
   public static void main(String [] args){
     Product p = new Product();
-    int result1 = p.product(2,3);
-    int result2 = p.product(2,5,7);
-    double result3 = p.product(2.5,3.5);
+    int result1 = p.Product(2,3);
+      System.out.println(result1);
+    int result2 = p.Product(2,5,7);
+      System.out.println(result2);
+    double result3 = p.Product(2.5,3.5);
+      System.out.println(result3);
   }
 }
 class Product{
-        public int product(int x,int y){
+        public int Product(int x,int y){
                 return x*y;
         }
-        public int product(int x,int y,int z){
+        public int Product(int x,int y,int z){
                 return x*y*z;
         }
-        public double product(double x,double y){
+        public double Product(double x,double y){
                 return x*y;
         }
 }
